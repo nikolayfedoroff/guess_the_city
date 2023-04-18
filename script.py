42d959c381b55e77467da5acdf50b282f45f31ee
Николай Федоров <nikolay.fedorov.2005.2213@gmail.com>
2023-04-18 15:19:13 +0300

    Completed script.py, added data.json

--- script.py
+++ script.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 import csv
+import json
 
 filename = 'city.csv'
 
 fields = []
-data = []
+rows = []
 
 with open(filename, 'r') as file:
 	csvreader = csv.reader(file)
@@ -13,4 +14,36 @@ with open(filename, 'r') as file:
 	fields = next(csvreader)
 
 	for row in csvreader:
-		data.append(row)
+		rows.append(row)
+
+fields[0] = 'name'
+
+required = ['name', 'capital_marker', 'geo_lat', 'geo_lon', 'population']
+
+idx = {}
+
+for item in required:
+	idx[item] = fields.index(item)
+
+for row in rows:
+	row[idx['name']] = row[idx['name']][row[idx['name']].find('г ') + 2:]
+
+for row in rows:
+	if (row[idx['capital_marker']] == '2'):
+		row[idx['capital_marker']] = 1
+	else:
+		row[idx['capital_marker']] = 0
+	
+
+data = []
+
+for row in rows:
+	data.append({});
+	data[-1]['name'] = row[idx['name']]
+	data[-1]['capital_marker'] = row[idx['capital_marker']]
+	data[-1]['geo_lat'] = float(row[idx['geo_lat']])
+	data[-1]['geo_lon'] = float(row[idx['geo_lon']])
+	data[-1]['population'] = int(row[idx['population']])
+
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile)
