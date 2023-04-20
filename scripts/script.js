5185875786f3469044956c07fd45e57908dee408
Николай Федоров <nikolay.fedorov.2005.2213@gmail.com>
2023-04-20 18:37:10 +0300

    Completed input div, js code beautified

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,9 @@
-var map = L.map('map', {attributionControl: false}, {zoomSnap: 100}, {inertia: false}).setView([55.788631, 37.466790], 10);
+var map = L.map('map', {
+    attributionControl: false
+}, {
+    zoomSnap: 100
+}).setView([55.788631, 37.466790], 10);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19}).addTo(map);
-
-var circle = L.circle([51.508, -0.11], {
-    color: 'green',
-    fillColor: '#f03',
-    fillOpacity: 1,
-    radius: 500
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19
 }).addTo(map);
